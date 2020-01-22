431667d05d59c7228d33f96b25538fda32cf1206
Miles Fearnall-Williams <mairuzu@gmail.com>
2020-01-23 00:41:23 +0900

    Organization ad prefabs done

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -11,15 +10,5 @@ public class MusicPlayer : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("LoadFirstScene", 2f);
-    }
 
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
