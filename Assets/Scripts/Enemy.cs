5fdbbe29a2e1aca1a2a1ca188106c68e719c4ea2
Miles Fearnall-Williams <mairuzu@gmail.com>
2020-01-26 01:47:41 +0900

    no message

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject explosionFX;
+    [SerializeField] Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddBoxCollider();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject deathFX = Instantiate(explosionFX, transform.position, Quaternion.identity);
+        deathFX.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
